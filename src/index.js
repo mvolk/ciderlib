b16762c70dbe70f214cc75f763bef665c6ed9781
Michael Volk <michael-volk@hotmail.com>
2017-01-15 18:10:50 -0800

    [exports] Add Hydrometer and temperature to module exports.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import Hydrometer from './instruments/Hydrometer';
+import temperature from './conversions/temperature';
 import water from './substances/water';
 
 module.exports = {
+  Hydrometer,
+  temperature,
   water,
 };
